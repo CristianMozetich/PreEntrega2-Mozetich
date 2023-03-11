e2a6c413d2c27bb3b720ec41026106c441465179
Cristian Mozetich <cristianmozetich@gmail.com>
2023-03-11 18:15:44 +0000

    preparando segunda entrega

--- script.js
+++ script.js
@@ -1,45 +1,83 @@
-function usuario(){
-    let nombreDeUsuario = prompt("Enter username");
-    console.log("Welcome " + nombreDeUsuario);
-    while (nombreDeUsuario === ""){
-        nombreDeUsuario = prompt("Wrong username, try again");
+function cliente(){
+    alert ("Bienvenido a PIG BANK");
+    let usuario = prompt ("Ingrese su nombre para continuar");
+    while (usuario === ""){
+        usuario = prompt("Ingrese su nombre para continuar");
+    }
+    return usuario
+}
+let usuario = cliente()
+
+
+function contraseña(){
+    let password = parseInt(prompt("Ingrese su contraseña"));
+    while (password === ""){
+        password = parseInt(prompt("Ingrese su contraseña para continuar"));
+    }
+    return password
+}
+let password = contraseña()
+
+
+class account{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+}
+const standard = new account ('standard', 5);
+const smart = new account ('smart', 10);
+const premium = new account ('premium', 20);
+
+
+const arrayAccounts = [standard, smart, premium];
+console.log (arrayAccounts);
+
+const divAccounts = document.getElementById ("container")
+arrayAccounts.forEach (account =>{
+    const div = document.createElement ("div");
+    div.innerHTML = `<p>Name: ${account.name} </p>
+                     <p>Price: ${account.price} </p>
+                     <button>Get this account</button>`
+    divAccounts.appendChild(div);
+});
+
+
+
+function welcome (){
+    let account = parseInt(prompt("¿Que tipo de cuenta le gustaria adquirir? \n 1) Cuenta Standard \n 2) Cuenta Smart \n 3) Cuenta Premium"));
+    while (account <1 || account >3){
+        account = parseInt(prompt("¿Que tipo de cuenta le gustaria adquirir? \n 1) Cuenta Standard \n 2) Cuenta Smart \n 3) Cuenta Premium"));
+    }    
+    monstrarDetalles (account);
+}
+
+function monstrarDetalles(account){
+    if (account === 1){
+        alert ("Usted selecciono cuenta Standard");
+        console.log(account)
+        console.log('ustede debe abonar ' + account.price);
+    } else if (account === 2){
+        alert ("Usted selecciono cuenta Smart");
+        console.log(account)
+        console.log('Usted debe abonar ' + account.price);
+    } else if (account === 3){
+        alert ("Usted selecciono cuenta Premium");
+        console.log(account)
+        console.log('Ustede debe abonar ' + account.price);
     }
 }
 
-usuario();
-
-function password(){
-    let contraseña = parseFloat(prompt("Enter your password"));
-    for (let i=0; i=5; i++){
-        if (contraseña === parseFloat(1234)){
-            alert ("Welcome to Pig Bank");
-            console.log(contraseña);
-            break;
-        } else{
-            contraseña = parseFloat(prompt("Wrong password, try again"));
-        }
-    }  
-}
-
-password();
-
-let money = parseFloat(prompt("¿How much money would you like to enter?"));
-if(money >20 && money <=500){
-    alert("The money entered is correctly");
-    console.log("correct amount");
-} else if(money <20 || money >500){
-    alert("That amount of money is not allowed, enter amount greater than 20 and less than 500");
-}
-console.log(money); 
+welcome();
 
 
-function costo(deposito, mantenimientoDeCuenta, interes){
-    let total = (deposito - mantenimientoDeCuenta)*interes;
-    alert ("The total money in your account is " + total);
-    console.log(total)
-}
-let num1 = money;
-let num2 = parseFloat(20);
-let num3 = parseFloat(1.5);
 
-costo(num1, num2, num3);
+
+
+
+
+
+
+
+
+
